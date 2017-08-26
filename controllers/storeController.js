b04b1db700ac9ededbf6af48dda5e40a9b19fc18
Vonetta <vonettastevenson@gmail.com>
2017-08-25 17:37:39 -0700

    added the view for add and edit

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -4,3 +4,7 @@ exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
 };
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store' });
+};
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ const { catchErrors } = require('./../handlers/errorHandlers');
 const router = express.Router();
 
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
 
 module.exports = router;
