36e88f3f72f08eee85ecbabd0d502175ba5c7b32
Lauren Tapley <laurentapley@Laurens-MBP.home>
2017-08-26 13:37:21 -0700

    add const Store and add exports.createStore to storeController

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
   console.log(req.name);
@@ -10,5 +11,5 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = (req, res) => {
-  res.json(req.body);
+   const store = new Store(req.body);
 };
