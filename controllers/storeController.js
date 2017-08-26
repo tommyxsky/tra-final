26a8d79d0c211e76f713ceedacaba43fe012bed8
Lauren Tapley <laurentapley@Laurens-MBP.home>
2017-08-26 13:51:10 -0700

    added ability to save to mongoDB

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -10,6 +10,8 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-   const store = new Store(req.body);
+exports.createStore = async (req, res) => {
+    const store = new Store(req.body);
+        await store.save();
+        res.redirect(`/`);
 };
