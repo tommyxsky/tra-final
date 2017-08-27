eec86a124946c376ad0adc7789bfe1427649dc99
Lauren Tapley <laurentapley@Laurens-MacBook-Pro.local>
2017-08-26 22:28:08 -0700

    complete edit store, updates database, updates store page

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const storeController = require('./../controllers/storeController');
-
 const { catchErrors } = require('./../handlers/errorHandlers');
 
 const router = express.Router();
 
+// Do work here
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.get('/add', storeController.addStore);
 router.post('/add', catchErrors(storeController.createStore));
+router.post('/add/:id', catchErrors(storeController.updateStore));
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 module.exports = router;
