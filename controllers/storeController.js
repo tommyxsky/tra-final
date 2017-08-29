4ceb0539e5ae9a265d09c7c30e864b66afa8b33f
Lauren Tapley <laurentapley@Laurens-MacBook-Pro.local>
2017-08-29 16:39:56 -0700

    added single store view

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -60,3 +60,11 @@ exports.updateStore = async (req, res) => {
   );
   res.redirect(`/stores/${store._id}/edit`);
 };
+
+exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+  if (!store) {
+    return next();
+  }
+    res.render('store', { store, title: store.name });
+};
--- helpers.js
+++ helpers.js
@@ -11,6 +11,9 @@ exports.moment = require('moment');
 // Dump is a handy debugging function we can use to sort of "console.log" our data
 exports.dump = obj => JSON.stringify(obj, null, 2);
 
+//Google map
+exports.staticMap = ([lng, lat]) => `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=800x150&key=${process.env.MAP_KEY}&markers=${lat},${lng}&scale=2`;
+
 // inserting an SVG
 exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
 
