d2f3fa42e2b0f5455b9787c6421e025c76f24fc9
Sheron Sherman <ssherman1003@gmail.com>
2017-09-03 16:18:57 -0700

    index organize

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,25 @@
+//Express is used for the app,
+//Store Controller allows us to create the model for the application,
+//Error Handlers is the file used to alert us to any mistakes or error if the project
+
 const express = require('express');
 const storeController = require('./../controllers/storeController');
 const { catchErrors } = require('./../handlers/errorHandlers');
-
 const router = express.Router();
 
-// Do work here
+// Router allows for the connection to the app and the different components
+
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
-
 router.get('/stores/page/:page', catchErrors(storeController.getStores));
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.get('/add', storeController.addStore);
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
+router.get('/tags', catchErrors(storeController.getStoresByTag));
+router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+// Router Post sends data that can't be seen in the data
 
 router.post(
   '/add',
@@ -28,6 +35,6 @@ router.post(
   catchErrors(storeController.updateStore)
 );
 
-router.get('/tags', catchErrors(storeController.getStoresByTag));
-router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+// Exports all of the router modules
+
 module.exports = router;
